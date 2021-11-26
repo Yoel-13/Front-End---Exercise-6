b7efd1e8cf0002ef125dfe584a9d04859c5ebf0a
Yoel-13 <s11810125@student.unklab.ac.id>
2021-11-27 00:53:31 +0800

    Front End - Exercise #6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect} from 'react'
+
+function apiCall(url,res,rej){
+  let datas = new XMLHttpRequest()
+  datas.onreadystatechange = function (){
+    if (datas.readyState === 4) {
+      if (datas.status === 200) {
+        res(datas.response)
+      } else {
+        rej()
+      }
+    }
+  }
+  datas.open('get',url)
+  datas.send()
+}
 
 function App() {
+  const [userInfo,setUserInfo] = useState([])
+  useEffect(()=>{
+    apiCall('https://reqres.in/api/users?page=2',res=>{
+      const datas = JSON.parse(res)
+      setUserInfo(datas.data)
+    })
+  },[])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 style={{color:'white'}}>User Lists</h1>
       </header>
+      <div className="container">
+        {
+          userInfo.map(item=>{
+            return(
+              <div key={item.id} className="userHolder">
+                <img src={item.avatar} alt="avatar"/>
+                <div style={{height:30,display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'row'}}>
+                  <h3 style={{marginRight:10}}>{item.first_name}</h3>
+                  <h3>{item.last_name}</h3>
+                </div>
+                <h5><cite>{item.email}</cite></h5>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
